00f090ed9384fd07a9cfbc886610e16742e283c4
ocnelavoc <ocnelavoc@gmail.com>
2023-12-12 23:05:52 +0200

    upd

--- pars.py
+++ pars.py
@@ -1,32 +1,7 @@
 import requests
 from bs4 import BeautifulSoup as bs
-import pandas as pd
 import os
-import numpy
 import csv
-
-
-import numpy as np
-# URL_TEMPLATE = "https://www.work.ua/ru/jobs-odesa/?page=2"
-# FILE_NAME = "test.csv"
-
-
-# def parse(url = URL_TEMPLATE):
-#     result_list = {'href': [], 'title': [], 'about': []}
-#     r = requests.get(url)
-#     soup = bs(r.text, "html.parser")
-#     vacancies_names = soup.find_all('h2', class_='add-bottom-sm')
-#     vacancies_info = soup.find_all('p', class_='overflow')
-#     for name in vacancies_names:
-#         result_list['href'].append('https://www.work.ua'+name.a['href'])
-#         result_list['title'].append(name.a['title'])
-#     for info in vacancies_info:
-#         result_list['about'].append(info.text)
-#     return result_list
-
-# data=parse()
-# df = pd.DataFrame(data)
-# df.to_csv(FILE_NAME)
 
 
 # Python3 code here creating class
@@ -35,8 +10,7 @@ class price_item:
         self.id = id
         self.name = name
         self.price = price
-        
-        
+       
 os.system('cls')
 url = 'https://hi-tech.md/kompyuternaya-tehnika/'
 # ['https://hi-tech.md/kompyuternaya-tehnika/page-2/']
@@ -55,7 +29,6 @@ while True:
             break    
     else:
         response = requests.get(url)
-    
     # print(response)
 
     soup = bs(response.text,"html.parser")
@@ -64,7 +37,6 @@ while True:
     except: 
         break   
     try:
-       
         for item in soup.find_all("div", {"class": "col-tile"}):
             id = item.find("span",{"class":"ty-control-group__item"}).text
             title= (item.find("a",{"class": "product-title"})).get('title')
