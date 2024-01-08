eb0e62be1af6383048be2057c94f2cd4aa80a6df
ocnelavoc <ocnelavoc@gmail.com>
2024-01-08 07:46:44 +0200

    fix

--- pars_zvezda.py
+++ pars_zvezda.py
@@ -7,8 +7,8 @@ from datetime import datetime
 
 # "Komplektujuschie","2873", "Kompjuternaja-mebel","bytovaya","ohrannye-sistemy","svet-i-jelektrika","uslugi","avtojelektronika","rashodnye-materialy","Подарочные сертификаты","Programmnoe-obespechenie"
 # "Komplektujuschie", "2873", "Kompjuternaja-mebel", "bytovaya", "ohrannye-sistemy", "svet-i-jelektrika",          "uslugi", "avtojelektronika", "rashodnye-materialy",
-razdel = ["kamen-4082", "novogodnij-dekor-4092", "suxie-stroitelnye-smesi-i-gidroizolyaciya-2263",
-          "teploizolyaciya-i-shumoizolyaciya-3919"]
+# razdel = ["kamen-4082", "novogodnij-dekor-4092", "suxie-stroitelnye-smesi-i-gidroizolyaciya-2263",
+#           "teploizolyaciya-i-shumoizolyaciya-3919"]
 
 
 # Python3 code here creating class
@@ -68,22 +68,39 @@ def save_to_csv(file_name, data_arr: [price_item_model]):
     print("Сохранение завершено, спасибо за работу.")
 
 
-def get_row(item):
+def get_row(item, category):
     """
     Получение строки данных из html карточки товара
     :param item:
     :return: price_item
     """
-    id_element = item.find("div", {"class": "quick-view"})
-    title_element = (item.find("p", {"class": "view-prod-name"}))
-    price_element = item.find("span", {"class": "woocommerce-Price-amount"})
-    sub_category_element = item.find("p", {"class": "view-prod-category"})
-    sub_category = sub_category_element.text if sub_category_element else "none"
-    id: str = id_element.get('data-prod') if id_element else " non"
-    title: str = title_element.get_text(separator=' ', strip=True) if title_element else "no title"
+    id_element = item.find("a",class_= "add_to_cart_button")
+    # print("id_element",id_element)
+    id: str = id_element['data-product_id'] if id_element else " non"
+    print("id:", id)
+    
+    title_element = (item.find("h2", class_="woocommerce-loop-product__title"))
+    title: str = title_element.text if title_element else "no title"
+    print("title", title)
+    
+    price_element = item.find("bdi")
     price: str = price_element.text if price_element else "0"
-    current_date: str = datetime.datetime.now().strftime('%d.%m.%Y')
-    return price_item(id, "category", sub_category, title, price, current_date)
+    print("price:",price)   
+
+    # sub_category_element = item.find("p", {"class": "view-prod-category"})
+    # category = soup.find("h1", {"class": "page-title"}).text
+    # sub_category = sub_category_element.text if sub_category_element else "none"
+
+    
+    
+ 
+    
+    current_date: str = datetime.now().strftime('%d.%m.%Y')
+    print("current_date1",current_date)
+    
+    res = price_item_model(id, "category", "category", title, price, current_date)
+    # print("res:",res)
+    return res
 
 
 def main(base_url):
@@ -102,8 +119,13 @@ def main(base_url):
     pages = 2
     category = ""
     current_date =  datetime.now()
+    count=0
     print("Парсинг начат ...")
     for cat in razdel:
+        print(len(razdel))
+        count =count + 1
+        if count==4:
+            break
         url = cat
         i = 1
         while True:
@@ -112,7 +134,7 @@ def main(base_url):
             #     break
             if i > 1:
                 try:
-                    test_url = url + "?page/" + str(i)
+                    test_url = url + "page/" + str(i)
 
                     
                 except:
@@ -134,10 +156,13 @@ def main(base_url):
                 # print(response)
 
             try:
-                items = soup.find_all("li", {"class": "product-small"})
-                print(items)
+                items = soup.find_all("li", class_="type-product")
+                print("количество", len(items))
                 for item in items:
-                    rows.append(get_row(item))
+                    r= get_row(item,category)
+                    # print("-----------------------------",item) 
+                    rows.append(r)
+                    # print("+")
 
                 if len(items) == 0:
                     break
@@ -146,6 +171,7 @@ def main(base_url):
                 print("Ошибка поиска карточки")
                 break
 
+    # print(rows)
     save_to_csv("data_zvezda.csv", rows)
 
 
